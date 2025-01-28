2dfe9074f08fc2c8d2ac2e83d3197e8f95be387a
JibLeekitwattana <pl2823@nyu.edu>
2025-01-28 14:27:36 -0500

    wasd script updated

--- CodeLab-S25-Week1/Assets/Scripts/WASDController.cs
+++ CodeLab-S25-Week1/Assets/Scripts/WASDController.cs
@@ -3,10 +3,13 @@ using UnityEngine;
 public class WASDController : MonoBehaviour
 {
     public KeyCode keyUp = KeyCode.W;
+    public KeyCode keyDown = KeyCode.S;
+    public KeyCode keyLeft = KeyCode.A;
+    public KeyCode keyRight = KeyCode.D;
 
     public Rigidbody rb;
 
-    public float moveForce = 10f;
+    public float moveForce = 2f;
 
     Vector3 testVec;
     
@@ -15,7 +18,7 @@ public class WASDController : MonoBehaviour
     {
         Debug.Log("START!!!");
 
-        rb = null;
+        //rb = null;
         
     }
 
@@ -26,7 +29,23 @@ public class WASDController : MonoBehaviour
 
         if (Input.GetKey(keyUp))
         {
-            rb.AddForce(testVec * moveForce);
+            //rb.AddForce(testVec * moveForce);
+            rb.AddForce(Vector3.up * moveForce);
+        }
+
+        if (Input.GetKey(keyDown))
+        {
+            rb.AddForce(Vector3.down * moveForce);
+        }
+
+        if (Input.GetKey(keyLeft))
+        {
+            rb.AddForce(Vector3.left * moveForce);
+        }
+
+        if (Input.GetKey(keyRight))
+        {
+            rb.AddForce(Vector3.right * moveForce);
         }
     }
 }
